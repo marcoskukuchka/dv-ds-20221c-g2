3d30843602dab08a8ffdccb20576c36e1f8d2501
Marcos <marcos.kukuchka@gmail.com>
2022-07-07 19:07:49 -0300

    Agregado del proyecto Java con SpringBoot

---
+++ src/main/java/ar/edu/davinci/dvds20221cg2/DvDs20221cG2Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20221cg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20221cG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20221cG2Application.class, args);
+	}
+
+}
