70115d26720692d3b3195e69134106bc1946ae81
Marcos <marcos.kukuchka@gmail.com>
2022-07-08 20:52:05 -0300

    add JUnit test and db scripts

--- src/main/java/ar/edu/davinci/dvds20221cg2/repository/PrendaRepository.java
+++ src/main/java/ar/edu/davinci/dvds20221cg2/repository/PrendaRepository.java
@@ -1,9 +1,10 @@
 package ar.edu.davinci.dvds20221cg2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ar.edu.davinci.dvds20221cg2.domain.Prenda;
-
+@Repository
 public interface PrendaRepository extends JpaRepository<Prenda, Long> {
 
 }
