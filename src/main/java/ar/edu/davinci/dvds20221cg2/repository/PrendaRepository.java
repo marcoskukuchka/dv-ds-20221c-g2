403ea53e390be01c0e87ccf7b978fcc8043fc139
Marcos <marcos.kukuchka@gmail.com>
2022-07-08 19:44:41 -0300

    add repository

---
+++ src/main/java/ar/edu/davinci/dvds20221cg2/repository/PrendaRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.davinci.dvds20221cg2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ar.edu.davinci.dvds20221cg2.domain.Prenda;
+
+public interface PrendaRepository extends JpaRepository<Prenda, Long> {
+
+}
