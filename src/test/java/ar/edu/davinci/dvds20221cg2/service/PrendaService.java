02d829fe9634049fb54cda9940362bce53a89d19
Marcos <marcos.kukuchka@gmail.com>
2022-07-10 16:59:53 -0300

    add exception and service

---
+++ src/test/java/ar/edu/davinci/dvds20221cg2/service/PrendaService.java
@@ -0,0 +1,32 @@
+package ar.edu.davinci.dvds20221cg2.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import ar.edu.davinci.dvds20221cg2.domain.Prenda;
+import ar.edu.davinci.dvds20221cg2.exception.BusinessException;
+
+public interface PrendaService {
+	
+	// Métodos de negocio sobre la entida Prenda
+
+	// Métodos de creación, modificación y eliminación de una prenda
+	Prenda save(Prenda prenda) throws BusinessException;
+	Prenda update(Prenda prenda) throws BusinessException;
+	void delete(Prenda prenda);
+	void delete(Long id);
+
+	// Métodos de búsqueda
+	Prenda findById(Long id) throws BusinessException;
+	
+	// Métodos de listado
+	List<Prenda> list();
+	Page<Prenda> list(Pageable pegeable);
+	
+	// Método de contar la cantidad de registros
+	long count();
+
+	
+}
